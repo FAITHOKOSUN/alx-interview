4059364371d83baa40603537cebc82c735c8d6f1
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-08-08 13:26:43 +0100

    asyn_func

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines a function that calculates the fewestnumber of 
-operations needed to result in exactly n H characters in the a given file
+Minimum operations
 """
 
 
 def minOperations(n):
     """
-    returns the fewest number of operations needed to result in 
-    exactly n H characters in a given file.
+
+    :param n:
+    :return:
     """
-    if type(n) is not int or n <= 1:
+    if n <= 1:
         return 0
-    summation = []
-    divisor = 2
-    while (n % divisor) is 0 and (n // divisor) is not 1:
-        summation.append(divisor)
-        n = n // divisor
-    divisor = 3
-    while n > divisor:
-        while (n % divisor) is 0 and (n // divisor) is not 1:
-            summation.append(divisor)
-            n = n // divisor
-        divisor += 2
-    summation.append(n)
-    return sum(summation)
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
